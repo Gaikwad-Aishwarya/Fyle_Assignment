e4aa2f7f56c4aea592604febcbc9377f8b1a32e1
Gaikwad-Aishwarya <aishwaryagaikwad1125@gmail.com>
2024-04-13 13:53:34 +0530

    Fyle Assignment completed

---
+++ src/app/repository-list/repository-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-repository-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './repository-list.component.html',
+  styleUrl: './repository-list.component.scss'
+})
+export class RepositoryListComponent {
+
+}
